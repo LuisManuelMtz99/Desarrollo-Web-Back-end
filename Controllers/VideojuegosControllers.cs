0da51313084ecf863d800873d47da8701e4ba0d3
LuisManuelMtz99 <ray_mp90@hotmail.com>
2022-09-28 21:55:20 -0500

    Se agrego 1.12 - Modificación de Controladores

--- Controllers/VideojuegosControllers.cs
+++ Controllers/VideojuegosControllers.cs
@@ -18,14 +18,62 @@ namespace WebApplicationBackEnd.Controllers
         }
 
         [HttpGet]
+        [HttpGet("listado")]
+        [HttpGet("/lisatado")]
+
 
         public async Task<ActionResult<List<Videojuego>>> Get()
         {
             return await dbContext.Videojuegos.Include(x => x.datos).ToListAsync();
         }
+
+
+        [HttpGet("primero")] //videojuegos/primero
+
+        public async Task<ActionResult<Videojuego>> PrimerAutor([FromHeader] int valor, [FromQuery] string videojuego, [FromQuery] int Videojuegoid)
+        {
+            return await dbContext.Videojuegos.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("primero2")] //videojuegos/primero
+
+        public  ActionResult<Videojuego> PrimerAutorD()
+        {
+            return new Videojuego() { nombre = "DOS" };
+        }
+
+        [HttpGet("{id:int}/{param=DbzBt3}")] 
+
+        public async Task<ActionResult< Videojuego>> Get(int Id, string param)
+        {
+           var videojuego =  await dbContext.Videojuegos.FirstOrDefaultAsync(x => x.id == Id);
+
+               if(videojuego == null)
+            {
+                return NotFound();
+           }
+
+            return videojuego;
+        }
+
+        [HttpGet("{Nombre}")]
+
+        public async Task<ActionResult<Videojuego>> Get([FromRoute] string Nombre)
+        {
+            var videojuego = await dbContext.Videojuegos.FirstOrDefaultAsync(x => x.nombre.Contains(Nombre));
+
+            if (videojuego == null)
+            {
+                return NotFound();
+            }
+
+            return videojuego;
+        }
+
+
         [HttpPost]
 
-        public async Task<ActionResult> Post(Videojuego videojuego)
+        public async Task<ActionResult> Post([FromBody]Videojuego videojuego)
         {
             dbContext.Add(videojuego);
             await dbContext.SaveChangesAsync();
--- Entidades/Videojuego.cs
+++ Entidades/Videojuego.cs
@@ -1,10 +1,27 @@
-﻿namespace WebApplicationBackEnd.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationBackEnd.Entidades
 {
     public class Videojuego
     {
         public int id { get; set; }
+        [Required (ErrorMessage ="El campo Nombre es requerido")]
+        [StringLength(maximumLength:15, ErrorMessage = "El campo nombre solo puede tener un maximo de 15 caracteres")]
 
         public string nombre { get; set; }
+
+        [Range(1952,2022, ErrorMessage ="El año no se encuentra entre el rango 1952 y 2022")]
+        [NotMapped]
+        public int año { get; set; }
+
+        [CreditCard]
+        [NotMapped]
+        public string tarjeta { get; set; }
+
+        [Url]
+        [NotMapped]
+        public string URL  { get; set; }    
 
         public List<Datos> datos { get; set; }
 
